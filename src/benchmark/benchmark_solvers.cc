353c531b37f976a2635d75463dc801dfee13befa
king-kong <yangqp5@outlook.com>
2019-05-31 11:47:30 +0800

    added benchmark code

--- src/benchmark/benchmark_solvers.cc
+++ src/benchmark/benchmark_solvers.cc
@@ -9,14 +9,18 @@
 class LinearProblemFixture : public benchmark::Fixture {
  public:
   void SetUp(const ::benchmark::State& state) {
-    std::tie(m, n, max) = state.range(0);
+    // std::tie(m, n, max) = state.range(0);
+    m = state.range(0);
+    n = state.range(1);
+    max = static_cast<double>(state.range(2));
     A.resize(m);
     for (int i = 0; i < m; ++i) A[i].resize(m + n);
     c.resize(m + n);
     b.resize(m);
 
     std::mt19937 gen(rd());
-    // The cost coefficients should range be larger than 0.0, to avoid unbound LPs
+    // The cost coefficients should range be larger than 0.0, to avoid unbound
+    // LPs
     std::uniform_real_distribution<double> dis(0.0, max);
 
     for (int i = 0; i < n; ++i) c[i] = dis(gen);
@@ -25,9 +29,9 @@ class LinearProblemFixture : public benchmark::Fixture {
       // Avoid 0.0, otherwise the LP problem is most likely fixed
       double tmp = dis(gen);
       while (tmp == 0.0) {
-        b = dis(gen);
+        tmp = dis(gen);
       }
-      b[i] = b;
+      b[i] = tmp;
     }
     // Identity matrix corresponding to the slack variables
     for (int i = 0; i < m; ++i) {
@@ -40,11 +44,11 @@ class LinearProblemFixture : public benchmark::Fixture {
     }
   }
 
-  void SetUp(::benchmark::State& state) { SetUp(state); }
+  // void SetUp(::benchmark::State& state) { SetUp(state); }
 
-  void TearDown(::benchmark::State& state) {}
+  // void TearDown(::benchmark::State& /* state */) {}
 
-  void TearDown(const ::benchmark::State& state) {}
+  void TearDown(const ::benchmark::State& /* state */) {}
 
  protected:
   // The internal data structure for the Linear Problem
@@ -58,24 +62,25 @@ class LinearProblemFixture : public benchmark::Fixture {
 
 BENCHMARK_DEFINE_F(LinearProblemFixture, BM_DenseJLPSolver)
 (benchmark::State& state) {
+  using namespace compute_tools;
   // invert to suit our solver for maximization problems
-  for (int i = 0; i < n; ++i)
-    c[i] = -c[i];
+  for (int i = 0; i < n; ++i) c[i] = -c[i];
   ProblemStatus status;
-  double objective_value = 0.0;
+  // double objective_value = 0.0;
   int num_iteration = 1000;
   std::vector<double> basic_solution;
   std::vector<int> basic_indices;
   for (auto _ : state) {
     std::tie(status, basic_solution, basic_indices) =
-        solve(A, b, c, num_iteration);
+        Solve(A, b, c, num_iteration);
   }
-  state.SetItemsProcessed(int64_t(state.iterations));
+  state.SetItemsProcessed(int64_t(state.iterations()));
 }
 
 static void CustomArguments(benchmark::internal::Benchmark* b) {
-  for (int i = 2; i <= 2048; i <<= 1)
-    b->Args({i, i, 100.0});
+  for (int i = 2; i <= 2048; i <<= 1) b->Args({i, i, 100});
 }
 
-BENCHMARK(BM_DenseJLPSolver)->Apply(CustomArguments);
+BENCHMARK_REGISTER_F(LinearProblemFixture, BM_DenseJLPSolver)
+    ->Apply(CustomArguments)
+    ->Unit(benchmark::kMillisecond);
